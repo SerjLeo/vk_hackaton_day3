0b8bb243fb57e2e99dbd7c64987a71d78df610b9
SerjLeoDev <sergejleontev111@gmail.com>
2020-09-10 02:13:35 +0300

    Updates

---
+++ precache-manifest.6b93f11ed4540f9f8fa33f10be8900a5.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0b20e19e912d6d83a602bb9cbe86ebc",
+    "url": "/vk_hackaton_day3/index.html"
+  },
+  {
+    "revision": "073b1fd1a2b0e5f6c9a3",
+    "url": "/vk_hackaton_day3/static/css/main.a1107254.chunk.css"
+  },
+  {
+    "revision": "ece8c704ab28d8ad7324",
+    "url": "/vk_hackaton_day3/static/js/2.9aaa8ee6.chunk.js"
+  },
+  {
+    "revision": "41b66b6fd6eecba581340794bb1831be",
+    "url": "/vk_hackaton_day3/static/js/2.9aaa8ee6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "073b1fd1a2b0e5f6c9a3",
+    "url": "/vk_hackaton_day3/static/js/main.24352582.chunk.js"
+  },
+  {
+    "revision": "6ba97e29c5f0d513c5ad",
+    "url": "/vk_hackaton_day3/static/js/runtime-main.60a5a59c.js"
+  },
+  {
+    "revision": "dfc54362fc2b0f7b99a4038b87dd9e64",
+    "url": "/vk_hackaton_day3/static/media/1.dfc54362.png"
+  },
+  {
+    "revision": "2048975925bd8553d8aaa5e1d9d1ffb1",
+    "url": "/vk_hackaton_day3/static/media/2.20489759.png"
+  },
+  {
+    "revision": "dbf82c38432d45de3c1c99f13c8b5fee",
+    "url": "/vk_hackaton_day3/static/media/3.dbf82c38.png"
+  },
+  {
+    "revision": "e671d53961f44185ee61256a0e6f13a5",
+    "url": "/vk_hackaton_day3/static/media/4.e671d539.png"
+  },
+  {
+    "revision": "35bcd06768ffafae4a5721b8f0c4fd8f",
+    "url": "/vk_hackaton_day3/static/media/burger.35bcd067.png"
+  },
+  {
+    "revision": "06277842ccc3f7c2a37085189dacdcbc",
+    "url": "/vk_hackaton_day3/static/media/check-symbol.06277842.svg"
+  },
+  {
+    "revision": "751f84c25a8d1ff159f499cbb9aa5bb7",
+    "url": "/vk_hackaton_day3/static/media/edit.751f84c2.svg"
+  },
+  {
+    "revision": "4fd2ba47b4f59d0a4382a8d81aae85d6",
+    "url": "/vk_hackaton_day3/static/media/kfc.4fd2ba47.png"
+  },
+  {
+    "revision": "60464810e8cfcde7a20fc19284c38a0e",
+    "url": "/vk_hackaton_day3/static/media/mcdac.60464810.png"
+  },
+  {
+    "revision": "1198ed41690cc0974aa8baa0e95ebd1b",
+    "url": "/vk_hackaton_day3/static/media/refresh-button.1198ed41.svg"
+  },
+  {
+    "revision": "4eb05fdd9f3d81f2f81cac5847e25cc9",
+    "url": "/vk_hackaton_day3/static/media/sub.4eb05fdd.png"
+  }
+]);
